347657a7bf688c81d8084e0c7e42472afa287813
Kriszta <matyi.kriszta@gmail.com>
2014-03-21 14:27:44 +0000

    merge pauls rails app into flat site

---
+++ assets/js/application.js
@@ -0,0 +1,20 @@
+
+$(document).ready(function() {
+  $('#aboutlink').click(function() {
+    $('html, body').animate({
+    scrollTop: $("#about").offset().top
+    }, 500);
+  });
+
+  $('#studentslink').click(function() {
+    $('html, body').animate({
+    scrollTop: $("#students").offset().top
+    }, 500);
+  });
+
+  $('#projectslink').click(function() {
+    $('html, body').animate({
+    scrollTop: $("#projects").offset().top
+    }, 500);
+  });
+})
